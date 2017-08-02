50cca1d901d1d3857e9bc468489e526366af1b13
Zac-L <Zachlichens@gmail.com>
2017-08-01 18:12:54 -0700

    changed some console log msgs

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ if ( toughQuestion === 'yes' | toughQuestion === 'y' ) {
 }
 
 var pokemonQuestion = prompt ('Is my favorite Pokemon Charizard?').toLowerCase();
-console.log( userName + ' picked: ' + pokemonQuestion );
+console.log( userName + ' picked: ' + pokemonQuestion + ', to my favorite pokemon being Charizard.' );
 
 if ( pokemonQuestion === 'no' | pokemonQuestion === 'y') {
   alert( 'That\'s correct. Wow I am super impressed, It is Sylveon!');
@@ -24,7 +24,7 @@ if ( pokemonQuestion === 'no' | pokemonQuestion === 'y') {
 }
 
 var birthPlace = prompt('Was I born in Portland?').toUpperCase();
-console.log();
+console.log('Does ' + userName + ' think I was born in Portland? ' + birthPlace + '!');
 
 if (birthPlace === 'YES' | birthPlace === 'y') {
   alert('haha ya, Portlandia ftw!');
@@ -42,7 +42,7 @@ if (coffee === 'no' | coffee === 'y') {
 }
 
 var skyDive = prompt( 'Have I been sky diving?').toLowerCase();
-console.log();
+console.log('Do you think I have been sky diving ' + userName + '? ' + skyDive);
 
 if ( skyDive === 'yes' | skyDive === 'y') {
   alert( 'Hell ya! Such an amazing experience, highly recommend.');
@@ -51,7 +51,7 @@ if ( skyDive === 'yes' | skyDive === 'y') {
 }
 
 var red = prompt( 'Is my favorite color Red?').toLowerCase();
-console.log();
+console.log(userName + ' Thinks red is my favorite color? ' + red);
 
 if ( red === 'yes' | red === 'y') {
   alert( 'No way! It is Sky Blue.');
